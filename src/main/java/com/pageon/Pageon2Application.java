c5a25c255f8417af3e2682f842777db82295efe0
A <A@58.230.56.33>
2022-07-13 21:51:47 +0900

    ㅁ

---
+++ src/main/java/com/pageon/Pageon2Application.java
@@ -0,0 +1,13 @@
+package com.pageon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pageon2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Pageon2Application.class, args);
+	}
+
+}
